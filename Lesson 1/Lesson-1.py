36a3e21a5a3dabfb40e6302f574b0fa3062c550e
williamwitcombe <103463329+williamwitcombe@users.noreply.github.com>
2022-08-31 10:31:36 +1200

    Add files via upload

---
+++ Lesson 1/Lesson-1.py
@@ -0,0 +1,27 @@
+# W: J Witcombe
+# Variables, output, labels, data types
+
+#variables
+name = "William"
+height = 1.79
+address = "170 Coutts Street"
+phone = "049391234"
+hasAllergy = False
+
+# A summary of this information should be printed out the screen. 
+print(name)
+print(height)
+print(address)
+print(phone)
+print(hasAllergy)
+
+# Clarifying the program
+# Height is in meters
+# Address to be stored as street number and street
+# Country not required
+# No detail of allergy type
+# Date to be displayed in context of a readable language
+
+# Design a solution / algorith 
+# The variable declared above are to be stored as data
+# These variables are draft and should be decided on before finalised
